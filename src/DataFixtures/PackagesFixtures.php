7f6853d7b30958eec37822999a3d29d1baab7521
JeanPierreF <jeanpierrefrancois49@gmail.com>
2024-04-17 16:23:53 +0200

    fixture 2

---
+++ src/DataFixtures/PackagesFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Packages;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class PackagesFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+
+        $this->createPackage('Carton A', 60, 30, 40, false, 2, 'Secteur A',$manager);
+        $this->createPackage('Carton B', 40, 30, 40, true, 1, 'Secteur B',$manager);
+        $this->createPackage('Carton C', 60, 30, 20, false, 2, 'Secteur A',$manager);
+        $this->createPackage('Carton D', 40, 30, 20, true, 1, 'Secteur B',$manager);
+
+        $manager->flush();
+    }
+
+    public function createPackage(string $reference, int $length, int $width, int $height, bool $palletizable = false, int $occupancy = 1, string $storage, ObjectManager $manager )
+    {
+        $packages = new Packages();
+        $packages->setReference($reference);
+        $packages->setLength($length);
+        $packages->setWidth($width);
+        $packages->setHeight($height);
+        $packages->setPalletizable($palletizable);
+        $packages->setOccupancy($occupancy);
+        $packages->setStorage($storage);
+
+        $manager->persist($packages);
+
+        return $packages;
+    }
+}
--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -2,14 +2,19 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Products;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class UsersFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
+        $product = new Products();
+
+        $product->setName('');
+        $product->setIdPackages($product->getId());
+
         // $manager->persist($product);
 
         $manager->flush();
