77df2d0767d70ef0e8a41ae5ad3efd4a77aef013
JeanPierreF <jeanpierrefrancois49@gmail.com>
2024-04-22 14:38:38 +0200

    Random Packages id

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Services\RandomPackages;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function index(): Response
+    public function index(RandomPackages $randomPackages): Response
     {
         return $this->render('main/index.html.twig', []);
     }
---
+++ src/Services/RandomPackages.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Packages;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**Sélectionne un carton au hasard dans la table 'Packages' est retourne son id.
+Cette classe sert pour remplir le champs 'id_packages' de la table 'Products' lors des fixtures */
+/**Selects a box at random from the 'Packages' table and returns its id.
+This class is used to fill the 'id_packages' field of the 'Products' table during fixtures */
+
+
+Class RandomPackages
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getRandomPackagesId(): ?int
+    {
+                // Récupérer un ID aléatoire à partir de la base de données
+                // Retrieve random ID from database
+                $repository = $this->entityManager->getRepository(Packages::class);
+                $packages = $repository->findAll();
+        
+                if (empty($packages)) {
+                    return null;
+                }
+        
+                // Choisir un carton aléatoire et récupérez son ID
+                // Choose a random box and get its ID
+                $randomPackage = $packages[array_rand($packages)];
+                return $randomPackage->getId();
+    }
+}
